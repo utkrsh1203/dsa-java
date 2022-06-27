f5704151b3495720ac9e2ad70939eb6c0dc8f479
utkrsh1203 <utkrsh2212@gmail.com>
2022-06-28 02:07:33 +0530

    hi

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World");
 
     }
 }
